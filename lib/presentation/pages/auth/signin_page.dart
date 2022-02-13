3e512175c239254f586d4cf9acb0945ad57b2d0e
Joseph Lugo <josephfronted1@gmail.com>
2022-02-13 11:59:01 -0500

    chore: view signin page add ui

--- lib/presentation/pages/auth/signin_page.dart
+++ lib/presentation/pages/auth/signin_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'package:flxtech/core/theme/colors_app.dart';
+import 'package:flxtech/core/values/dimensions.dart';
+import 'package:flxtech/generated/l10n.dart';
+import 'package:flxtech/presentation/widgets/buttons/custom_text_button.dart';
+import 'package:flxtech/presentation/widgets/buttons/theme_button.dart';
+import 'package:flxtech/presentation/widgets/custom_text_formfield.dart';
 import 'package:flxtech/presentation/widgets/scaffold_container.dart';
 
 class SignInPage extends StatelessWidget {
@@ -6,11 +14,77 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final S l10n = S.current;
     return ScaffoldContainer(
-      'sign in page',
+      l10n.signIn,
       isWavefooter: true,
-      body: Center(
-        child: Text('SignIn Page'),
+      body: Container(
+        height: MediaQuery.of(context).size.height * 0.7,
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(MARGIN_SIZE_LARGE),
+              child: Image.asset(
+                'assets/brand.png',
+                color: purpleColor,
+              ),
+            ),
+            Expanded(child: const SizedBox()),
+            CustomTextFormField(
+              hint: l10n.email,
+              rightIcon: Icons.alternate_email_outlined,
+            ),
+            CustomTextFormField(
+              hint: l10n.password,
+              rightIcon: Icons.remove_red_eye_outlined,
+            ),
+            Expanded(child: const SizedBox()),
+            ThemeButton(
+              onPress: () => null,
+              title: l10n.signIn,
+            ),
+            Expanded(child: const SizedBox()),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: Column(
+                children: [
+                  CustomTextButton(
+                    l10n.needAnAccount,
+                    color: orangeColor,
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                  // const Divider(color: whiteColor, height: 1.2),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 3,
+                      addAutomaticKeepAlives: true,
+                      shrinkWrap: true,
+                      itemExtent: MediaQuery.of(context).size.width/5,
+                      padding: const EdgeInsets.all(0),
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) => Container(
+                        margin: const EdgeInsets.only(
+                          right: MARGIN_SIZE_SMALL,
+                          left: MARGIN_SIZE_SMALL,
+                        ),
+                        child: SvgPicture.asset(
+                          'assets/icons/fb.svg',
+                          color: whiteColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  ThemeButton(
+                    title: l10n.signUp,
+                    onPress: () => null,
+                    buttonColor: whiteColor,
+                    textColor: primaryColor,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
