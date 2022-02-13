7c86bf0321b05444e36271da912459e2f85a324a
Joseph Lugo <josephfronted1@gmail.com>
2022-02-13 09:03:09 -0500

    widget: create custom floating app bar

---
+++ lib/presentation/widgets/custom_floating_appbar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:flxtech/core/theme/colors_app.dart';
+import 'package:flxtech/core/values/dimensions.dart';
+
+class CustomFloatingAppBar extends StatelessWidget {
+  const CustomFloatingAppBar({
+    required this.title,
+    this.rightPressed,
+    this.haveReturn = false,
+    required this.toltip,
+  });
+  final String title;
+  final double barHeight = BAR_HEIGHT;
+  final void Function()? rightPressed;
+  final bool haveReturn;
+  final String toltip;
+
+  Widget _getRightButton() => IconButton(
+    icon: Icon(Icons.help_outline_rounded, color: whiteColor),
+    onPressed: rightPressed,
+    tooltip: toltip,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    final double statusbarHeight = MediaQuery.of(context).padding.top;
+    return Container(
+      height: statusbarHeight + barHeight,
+      width: width,
+      padding: EdgeInsets.only(top: statusbarHeight),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [primaryColor, primaryColor]),
+      ),
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.centerLeft,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: _getRightButton(),
+          ),
+        ],
+      ),
+    );
+  }
+}
