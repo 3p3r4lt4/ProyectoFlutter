6bee4e75b3d445b7e65a594bf7d98219172b5ba7
Joseph Lugo <josephfronted1@gmail.com>
2022-02-13 09:02:23 -0500

    chore: add dimensions app

---
+++ lib/core/values/dimensions.dart
@@ -0,0 +1,49 @@
+  const double FONT_SIZE_EXTRA_SMALL = 10.0;
+  const double FONT_SIZE_SMALL = 12.0;
+  const double FONT_SIZE_DEFAULT = 14.0;
+  const double FONT_SIZE_LARGE = 16.0;
+  const double FONT_SIZE_EXTRA_LARGE = 18.0;
+  const double FONT_SIZE_OVER_LARGE = 24.0;
+
+  const double PADDING_SIZE_EXTRA_SMALL = 5.0;
+  const double PADDING_SIZE_SMALL = 10.0;
+  const double PADDING_SIZE_DEFAULT = 15.0;
+  const double PADDING_SIZE_LARGE = 20.0;
+  const double PADDING_SIZE_EXTRA_LARGE = 25.0;
+  const double PADDING_SIZE_XXLARGE = 30.0;
+  const double PADDING_SIZE_XXXLARGE = 35.0;
+  const double PADDING_SIZE_XXXLL = 40.0;
+
+  const double MARGIN_SIZE_EXTRA_SMALL = 5.0;
+  const double MARGIN_SIZE_SMALL = 10.0;
+  const double MARGIN_SIZE_DEFAULT = 15.0;
+  const double MARGIN_SIZE_LARGE = 20.0;
+  const double MARGIN_SIZE_EXTRA_LARGE = 25.0;
+  const double MARGIN_SIZE_XXLARGE = 30.0;
+  const double MARGIN_SIZE_XXXLARGE = 35.0;
+  const double MARGIN_SIZE_XXXLL = 40.0;
+  const double MARGIN_SIZE_XXXLLL = 50.0;
+  const double MARGIN_SIZE_XXXLLLL = 65.0;
+
+  const double ICON_SIZE_EXTRA_SMALL = 12.0;
+  const double ICON_SIZE_SMALL = 18.0;
+  const double ICON_SIZE_DEFAULT = 24.0;
+  const double ICON_SIZE_LARGE = 32.0;
+  const double ICON_SIZE_EXTRA_LARGE = 40.0;
+  const double ICON_SIZE_XXLARGE = 80.0;
+  const double ICON_SIZE_XXXLARGE = 120.0;
+
+  const double RADIUS_SIZE_EXTRA_SMALL = 2.5;
+  const double RADIUS_SIZE_SMALL = 5.0;
+  const double RADIUS_SIZE_DEFAULT = 7.5;
+  const double RADIUS_SIZE_LARGE = 10.0;
+  const double RADIUS_SIZE_EXTRA_LARGE = 15.0;
+  const double RADIUS_SIZE_XXLARGE = 20.0;
+  const double RADIUS_SIZE_XXXLARGE = 25.0;
+  const double RADIUS_SIZE_XXXLL = 30.0;
+  const double RADIUS_SIZE_XXXLLL = 35.0;
+
+  const double BAR_HEIGHT = 56.0;
+
+  const double IMAGE_HEIGHT_MEDIUM = 50.0;
+  const double IMAGE_HEIGHT_LARGE = 90.0;
