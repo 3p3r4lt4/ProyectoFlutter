a3497010b04dd8b5a6c553ad8b1cbe758ef7bb25
Joseph Lugo <josephfronted1@gmail.com>
2022-02-13 12:37:44 -0500

    config: create routes app

---
+++ lib/core/router/routes.dart
@@ -0,0 +1,7 @@
+abstract class Routes {
+  static const String WELCOME = '/welcome';
+  static const String SIGNIN = '/signin';
+  static const String QUOTER = '/quoter';
+  static const String WRAPPER = '/wrapper';
+  
+}
