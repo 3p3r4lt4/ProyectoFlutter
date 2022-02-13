d0f80e3e3107b061c32257a5e65d56980d1d3e6d
Joseph Lugo <josephfronted1@gmail.com>
2022-02-13 10:53:45 -0500

    config: add loacalizations in root app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'package:flxtech/generated/l10n.dart';
 import 'package:flxtech/presentation/pages/auth/signin_page.dart';
 
 void main() {
@@ -13,6 +15,13 @@ class FLXTech extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SignInPage(),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
     );
   }
 }
