625095fbde6162c83e710c9ad3673d2a855f3a99
Joseph Lugo <josephfronted1@gmail.com>
2022-02-13 09:59:25 -0500

    chore: add home sign in page in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flxtech/presentation/pages/auth/signin_page.dart';
+
 void main() {
   runApp(const FLXTech());
 }
@@ -9,8 +11,8 @@ class FLXTech extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: SignInPage(),
     );
   }
 }
