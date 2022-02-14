489fd0fca8b985fc50c6baa96365e00a19ae22bf
Joseph Lugo <josephfronted1@gmail.com>
2022-02-14 11:02:57 -0500

    chore: add local currency type in environment

--- lib/core/env/environment.dart
+++ lib/core/env/environment.dart
@@ -8,3 +8,4 @@ const double MAX_DISCOUNT_RANGE = 0.2; //20%
 const double MAX_DISCOUNT_RANGE_VALUE = 0.8; //80%
 const int ITERATIONS_RANGE_FEED = 12;
 const int ITERATIONS_QUANITY_FEED = 142;
+const String CURRENCY_TYPE_SYMBOL = 'S/';
