acb51192c75798c01c60bff170f9af54f9a2f739
Joseph Lugo <josephfronted1@gmail.com>
2022-02-14 14:01:23 -0500

    chore: add actions to use handle actions in quoter server item

--- lib/presentation/pages/quote/widgets/quoter_server_item.dart
+++ lib/presentation/pages/quote/widgets/quoter_server_item.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 
 import 'package:flxtech/core/style/text_style_app.dart';
-import 'package:flxtech/core/theme/colors_app.dart';
 import 'package:flxtech/core/values/dimensions.dart';
-import 'package:flxtech/generated/l10n.dart';
 import 'package:flxtech/presentation/controllers/quote/quoter_controller.dart';
 import 'package:flxtech/presentation/widgets/theme_text_formfield.dart';
 
 class QuoterServerItem extends StatelessWidget {
   const QuoterServerItem(
-    this.service, {
+    this.keyService, {
     this.onTap,
-    required this.keyService,
+    required this.service,
+    required this.controller,
   });
 
   final Map<String, dynamic> service;
   final Function? onTap;
   final String keyService;
+  final QuoteController controller;
 
   Widget _buildTextFormField(
     String label, 
@@ -30,14 +29,16 @@ class QuoterServerItem extends StatelessWidget {
       String? initialValue,
       TextInputType textInputType = TextInputType.number,
     }) =>
-    Column(
+    Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          margin: const EdgeInsets.only(left: MARGIN_SIZE_SMALL),
-          child: Text(
-            label,
-            style: TextStyleApp.caption(),
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.only(left: MARGIN_SIZE_SMALL),
+            child: Text(
+              label,
+              style: TextStyleApp.caption(),
+            ),
           ),
         ),
         ThemeTextFormField(
@@ -51,49 +52,15 @@ class QuoterServerItem extends StatelessWidget {
       ],
     );
 
-  Widget _buildEditServiceItem(BuildContext context, S l10n) {
-    return Column(
-      children: [
-        const Divider(
-          height: MARGIN_SIZE_LARGE, 
-          color: grayColor,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Consumer<QuoteController>(
-              builder: (context, controller, _) {
-                return _buildTextFormField(
-                  service['name'],
-                  context,
-                  initialValue: service['quantity'].toString(),
-                  onChanged: (String value) {
-                    controller.loadBagOfMinutesQuote(keyService, value);
-                  }
-                );
-              }
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget _buildContent(context, l10n) {
-
-    return Container(
-      margin: const EdgeInsets.only(left: MARGIN_SIZE_DEFAULT),
-      width: MediaQuery.of(context).size.width - MARGIN_SIZE_DEFAULT * 2,
-      child: _buildEditServiceItem(context, l10n)
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    final S l10n = S.current;
-    return InkWell(
-      onTap: () => onTap,
-      child: _buildContent(context, l10n),
+    return _buildTextFormField(
+      service['name'],
+      context,
+      initialValue: service['quantity'].toString(),
+      onChanged: (String value) {
+        if (value.isNotEmpty) controller.loadServicesQuote(keyService, value);
+      }
     );
   }
 }
