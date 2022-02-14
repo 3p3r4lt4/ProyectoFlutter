116b08b55a3fd3cde33c2d36b5feb2a914e9a403
Joseph Lugo <josephfronted1@gmail.com>
2022-02-14 09:23:11 -0500

    chore: add build data row feed dynamic in quoter app

--- lib/presentation/pages/quote/quoter_page.dart
+++ lib/presentation/pages/quote/quoter_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:flxtech/core/env/environment.dart';
+import 'package:flxtech/core/helpers/double_to_as_fixed_decimals.dart';
 import 'package:flxtech/core/style/text_style_app.dart';
 import 'package:flxtech/core/values/dimensions.dart';
 import 'package:flxtech/generated/l10n.dart';
@@ -10,6 +12,40 @@ import 'package:flxtech/presentation/widgets/theme_dropdown.dart';
 
 class QuoterPage extends StatelessWidget {
   const QuoterPage();
+
+  DataRow _buildDataRowFeed(
+      String key, String destination, QuoteController controller) {
+    return DataRow(
+      cells: [
+        DataCell(
+          Text(destination, style: TextStyleApp.b2()),
+        ),
+        DataCell(
+          ThemeDropDown(
+            paddingHorizontal: PADDING_SIZE_SMALL,
+            listItems:
+                List.generate(controller.quantityFeedMap.length, (index) {
+              final int quantityFeed = controller.quantityFeedMap[index]!;
+              return DropdownMenuItem(
+                value: '$quantityFeed',
+                child: Text(quantityFeed.toString()),
+              );
+            }),
+            valueSelected:
+                controller.bagOfMinutesQuote[key]!['quantity'].toString(),
+            onChanged: (dynamic value) {
+              controller.loadBagOfMinutesQuote(key, value);
+            },
+          ),
+        ),
+        DataCell(
+          Text(
+              '${doubleToAsFixedDecimals(controller.bagOfMinutesQuote[key]!['price'])}',
+              style: TextStyleApp.b2()),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,32 +59,35 @@ class QuoterPage extends StatelessWidget {
       body: ListView(
         children: [
           Container(
-            child: Text(
-              l10n.bagOfMinutes,
-              style: TextStyleApp.h1()
-            ),
+            alignment: Alignment.center,
+            child: Text(l10n.bagOfMinutes, style: TextStyleApp.h1()),
           ),
-          Consumer<QuoteController>(
-            builder: (context, controller, _) {
-              final String key = 'local_landline';
-              // print(controller.quantityFeedMap);
-              return ThemeDropDown(
-                isExpanded: true,
-                paddingHorizontal: PADDING_SIZE_LARGE,
-                listItems: List.generate(controller.quantityFeedMap.length, (index) {
-                  final int quantityFeed = controller.quantityFeedMap[index]!;
-                  return DropdownMenuItem(
-                    value: '$quantityFeed',
-                    child: Text(quantityFeed.toString()),
+          Consumer<QuoteController>(builder: (context, controller, _) {
+            // print(controller.bagOfMinutesQuote);
+            // final String key = 'local_landline';
+            return DataTable(
+              sortColumnIndex: 2,
+              dataRowHeight: HEIGHT_DATAROW,
+              // columnSpacing: 100,
+              columns: List.generate(
+                3,
+                (index) {
+                  List<String> titleList = [
+                    l10n.destination,
+                    l10n.quantity,
+                    l10n.price
+                  ];
+                  return DataColumn(
+                    label: Text(titleList[index], style: TextStyleApp.b2()),
                   );
-                }),
-                valueSelected: controller.bagOfMinutesQuote[key]!['quantity'].toString(),
-                onChanged: (dynamic value) {
-                  controller.loadBagOfMinutesQuote(key,value);
                 },
-              );
-            }
-          )
+              ),
+              rows: [
+                _buildDataRowFeed(
+                    'local_landline', l10n.localLandline, controller),
+              ],
+            );
+          }),
         ],
       ),
     );
