d598fa5c4c71609fbed883af6826416ea4e979d3
Joseph Lugo <josephfronted1@gmail.com>
2022-02-14 00:21:46 -0500

    chore: create name with feed map in dictionaries

--- lib/data/local/dictionaries.dart
+++ lib/data/local/dictionaries.dart
@@ -13,3 +13,48 @@ Map<String, double>  percentageDiscountMap = {
   '10': 0.15,
   '11': 0.20
 };
+
+//  S/.0.0459 S/.0.0954 S/.0.1369 S/.0.9350 S/.0.2499 S/.0.4921 S/.1.0000 S/.1.5957 S/.6.3500 S/.10.0000
+// Fijo Local	Fijo Nacional	Móviles	Fijo Rural	LDi1	LDi2	LDi3	LDi4	LDi5	LDi  Espercial
+Map<String, Map<String, dynamic>> nameWithFeedMap = {
+  'local_landline': {
+    'name': 'Fijo Local',
+    'feed': 0.0459
+  },
+  'national_landline': {
+    'name': 'Fijo Nacional',
+    'feed': 0.0954
+  },
+  'mobile': {
+    'name': 'Móviles',
+    'feed': 0.1369
+  },
+  'rural_landline': {
+    'name': 'Fijo Rural',
+    'feed': 0.9350
+  },
+  'ldi1': {
+    'name': 'LDi1',
+    'feed': 0.2499
+  },
+  'ldi2': {
+    'name': 'LDi2',
+    'feed': 0.4921
+  },
+  'ldi3': {
+    'name': 'LDi3',
+    'feed': 1.0000
+  },
+  'ldi4': {
+    'name': 'LDi4',
+    'feed': 1.5957
+  },
+  'ldi5': {
+    'name': 'LDi5',
+    'feed': 6.3500
+  },
+  'ldi_special': {
+    'name': 'LDi Espercial',
+    'feed': 10.0000
+  }
+};
