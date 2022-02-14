914c75ea07633b3a0239e403e59ae2e08062eacf
Joseph Lugo <josephfronted1@gmail.com>
2022-02-14 00:24:02 -0500

    chore: add load quantity and range ffed maps in auth controller

--- lib/presentation/controllers/auth/auth_controller.dart
+++ lib/presentation/controllers/auth/auth_controller.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 import 'package:flxtech/core/router/routes.dart';
+import 'package:flxtech/presentation/controllers/quote/quoter_controller.dart';
 
 class AuthController extends ChangeNotifier {
 
   void handleSignIn(BuildContext context) {
+      context.read<QuoteController>().loadQuantityFeedMap();
+      context.read<QuoteController>().loadRangeFeedMap();
     WidgetsBinding.instance!.addPostFrameCallback((_) {
-      print('success login');
       Navigator.pushNamedAndRemoveUntil(
       context,
       Routes.QUOTER,
