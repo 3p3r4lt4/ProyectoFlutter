9a8bd1e19c64163a6995a9ca1314c0da576fb11f
Joseph Lugo <josephfronted1@gmail.com>
2022-02-23 18:24:22 -0500

    chore: add menu ir router

--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -1,7 +1,8 @@
 abstract class Routes {
   static const String WELCOME = '/welcome';
-  static const String SIGNIN = '/signin';
+  static const String SIGN_IN = '/sign_in';
   static const String QUOTER = '/quoter';
   static const String WRAPPER = '/wrapper';
+  static const String MENU = '/menu';
   
 }
