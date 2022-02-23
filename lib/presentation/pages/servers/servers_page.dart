9c04c99c3759870f15b1b28c9cf47e3f7ad67916
Joseph Lugo <josephfronted1@gmail.com>
2022-02-23 18:30:39 -0500

    chore: create servers page init

---
+++ lib/presentation/pages/servers/servers_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:flxtech/core/style/text_style_app.dart';
+import 'package:flxtech/generated/l10n.dart';
+import 'package:flxtech/presentation/widgets/scaffold_container.dart';
+
+class ServersPage extends StatelessWidget {
+  const ServersPage();
+
+  @override
+  Widget build(BuildContext context) {
+    final S l10n = S.current;
+    return ScaffoldContainer(
+      l10n.servers,
+      body: Center(
+        child: Text(
+          l10n.servers,
+          style: TextStyleApp.b1(),
+        ),
+      ),
+    );
+  }
+}
