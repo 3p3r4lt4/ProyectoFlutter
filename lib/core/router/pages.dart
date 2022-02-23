9b8f5e6410658ffd8681e48add1d0541aec71f69
Joseph Lugo <josephfronted1@gmail.com>
2022-02-23 18:24:30 -0500

    chore: add menu ir pages

--- lib/core/router/pages.dart
+++ lib/core/router/pages.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/widgets.dart';
 
 import 'package:flxtech/core/router/routes.dart';
-import 'package:flxtech/presentation/pages/auth/signin_page.dart';
+import 'package:flxtech/presentation/pages/auth/sign_in_page.dart';
+import 'package:flxtech/presentation/pages/menu/user_menu.dart';
 import 'package:flxtech/presentation/pages/quote/quoter_page.dart';
 
 abstract class Pages {
-  static const String INITIAL = Routes.SIGNIN;
+  static const String INITIAL = Routes.SIGN_IN;
 
   static final Map<String, Widget Function(BuildContext)> routes = {
 
-    Routes.SIGNIN: (_) => const SignInPage(),
+    Routes.SIGN_IN: (_) => const SignInPage(),
     Routes.QUOTER: (_) => const QuoterPage(),
+    Routes.MENU: (_) => const UserMenu(),
   };
 }
     
