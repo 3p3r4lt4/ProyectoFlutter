ff1bc8ff1b344a4071868c3eef06e95f540ff3e8
Joseph Lugo <josephfronted1@gmail.com>
2022-02-23 18:31:46 -0500

    chore: create user menu

---
+++ lib/presentation/pages/menu/user_menu.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:flxtech/data/local/dictionaries.dart';
+import 'package:flxtech/presentation/pages/menu/menu.dart';
+import 'package:flxtech/presentation/pages/menu/pages_nav.dart';
+
+class UserMenu extends StatelessWidget {
+  const UserMenu();
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic> _barItemsMap = pagesItemBarMap;
+
+    return Menu(
+      PagesNav.listBottomNavBar(),
+      List<BottomNavigationBarItem>.generate(_barItemsMap.length, (index) {
+        final String key = _barItemsMap.keys.elementAt(index);
+        return PagesNav.userBarItem(
+          _barItemsMap[key]['icon'],
+          _barItemsMap[key]['title']
+        );
+      }),
+    );
+  }
+}
