40548ec1f490106e978ba0815bd3f56641b02692
Joseph Lugo <josephfronted1@gmail.com>
2022-02-25 01:12:29 -0500

    chore: create circuit repository to use get all circuits and get circuit calls

---
+++ lib/domain/repositories/circuit_repository.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:flxtech/core/errors/client_error.dart';
+import 'package:flxtech/domain/entities/call.dart';
+import 'package:flxtech/domain/entities/circuit.dart';
+import 'package:flxtech/domain/entities/request/bean_generic.dart';
+
+abstract class ClientRepository {
+  Future<Either<ClientError, Map<String, Circuit>>> getAllCircuits(BeanGeneric bean);
+
+  Future<Either<ClientError, Map<String,Call>>> getCircuitCalls(BeanGeneric bean);
+
+}
