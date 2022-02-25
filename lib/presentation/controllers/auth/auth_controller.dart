09b238b58bea0b2a1e4c926de7752b811225f652
Joseph Lugo <josephfronted1@gmail.com>
2022-02-25 09:40:51 -0500

    chore: add load all circuits in handle sign in

--- lib/presentation/controllers/auth/auth_controller.dart
+++ lib/presentation/controllers/auth/auth_controller.dart
@@ -2,17 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
+import 'package:flxtech/presentation/controllers/circuit/circuits_controller.dart';
 import 'package:flxtech/core/router/routes.dart';
 import 'package:flxtech/presentation/controllers/quote/quoter_controller.dart';
 
 class AuthController extends ChangeNotifier {
 
-  void handleSignIn(BuildContext context) {
+  void handleSignIn(BuildContext context) async {
       context.read<QuoteController>().loadQuantityFeedMap();
       context.read<QuoteController>().loadRangeFeedMap();
       context.read<QuoteController>().loadBagOfMinutesQuoteInitial();
       context.read<QuoteController>().loadServicesQuoteInitial();
       context.read<QuoteController>().loadServersEditingControllers();
+      await context.read<CircuitController>().loadAllCircuits(context);
     WidgetsBinding.instance!.addPostFrameCallback((_) {
       Navigator.pushNamedAndRemoveUntil(
       context,
