750c8d9096b4c793d4ccfa623ffe8ed336a00523
Joseph Lugo <josephfronted1@gmail.com>
2022-02-25 09:01:53 -0500

    chore: add feat det circuits all in injector container

--- lib/injector_container.dart
+++ lib/injector_container.dart
@@ -4,6 +4,13 @@ import 'package:flutter/widgets.dart' show WidgetsFlutterBinding;
 import 'package:flxtech/presentation/controllers/auth/auth_controller.dart';
 import 'package:flxtech/presentation/controllers/menu/menu_controller.dart';
 import 'package:flxtech/presentation/controllers/quote/quoter_controller.dart';
+import 'package:flxtech/presentation/controllers/circuit/circuits_controller.dart';
+
+import 'package:flxtech/data/repositories/circuit_repository_impl.dart';
+import 'package:flxtech/data/datasource/remote/clients_remote_datasource.dart';
+
+import 'package:flxtech/domain/repositories/circuit_repository.dart';
+import 'package:flxtech/domain/usecase/get_circuits_all.dart';
 
 final sl = GetIt.instance;
 
@@ -18,17 +25,24 @@ Future<void> init() async {
   sl.registerFactory(
     () => MenuController(),
   );
-  // to plain products
+  // to plain quoter
   sl.registerFactory(
     () => QuoteController(),
   );
+  // to plain circuit and client
+  sl.registerFactory(
+    () => CircuitController(
+      getCircuitsAll: sl(),
+    ),
+  );
 
   // * Data
-  
+  sl.registerLazySingleton<CircuitRepository>(() => CircuitRepositoryImpl(remoteDataSource: sl()));
   // * Network
-
+  sl.registerLazySingleton<CircuitsRemoteDataSource>(() => CircuitsRemoteDataSourceImpl());
   // * Local
 
   // * Domain use case
+  sl.registerFactory<GetCircuitsAll>(() => GetCircuitsAll(circuitRepository: sl()));
 
 }
